dfd1a1468c4a279fde7f199b5bc267db1e63ef64
0a71a5@gmail.com <0a71a5@gmail.com@80cae8be-53a3-4c99-4711-8fd00a4bf4d7>
2011-10-13 22:30:24 +0000

    deutsch/test minor mods

--- trunk/branches/deutsch/test/firmware/include/global.h
+++ trunk/branches/deutsch/test/firmware/include/global.h
@@ -3,6 +3,28 @@
 
 #define RECEIVE_TEST
 
+#ifdef IMMEDONGLE
+    #define LED_RED   P2_3
+    #define LED_GREEN P2_4
+    #define SLEEPTIMER  1200
+    
+#elif defined DONSDONGLES
+    // CC1111 USB Dongle
+    #define LED_RED   P1_1
+    #define LED_GREEN P1_1
+    #define SLEEPTIMER  1200
+    #define CC1111EM_BUTTON P1_2
+
+#else
+    // CC1111 USB (ala Chronos watch dongle), we just need LED
+    #define LED_RED   P1_0
+    #define LED_GREEN P1_0
+    #define SLEEPTIMER  1200
+#endif
+
+#define LED     LED_GREEN
+
+#define blink( on_cycles, off_cycles)  {LED=1; sleepMillis(on_cycles); LED=0; sleepMillis(off_cycles);}
 #define SLEEPTIMER  1200
 
 void sleepMillis(int ms);
