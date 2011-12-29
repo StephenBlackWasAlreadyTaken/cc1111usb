085d7f5f712f891f0f6d1503ab50d626b317ad3d
0a71a5@gmail.com <0a71a5@gmail.com@80cae8be-53a3-4c99-4711-8fd00a4bf4d7>
2011-12-29 02:57:43 +0000

    updating fhss

---
+++ trunk/branches/fhss/firmware/include/nic.h
@@ -0,0 +1,9 @@
+#define NIC_SET_CHANNELS        0x10
+#define NIC_NEXT_CHANNEL        0x11
+#define NIC_CHANGE_CHANNEL      0x12
+
+#define NIC_START_HOPPING       0x15
+#define NIC_STOP_HOPPING        0x16
+#define NIC_SET_MAC_THRESHOLD   0x17
+
+#define NIC_SET_ID              0x20
