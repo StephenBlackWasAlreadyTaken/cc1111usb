247f3530f07da7ea775ead61ed2341c427eadc74
0a71a5@gmail.com <0a71a5@gmail.com@80cae8be-53a3-4c99-4711-8fd00a4bf4d7>
2011-12-30 21:48:37 +0000

    added Serial Number incrementing at each firmware build, moved usb descriptor into cc1111usb.c, clean up and some merging from branches/fhss/

--- trunk/trunk/firmware/include/cc1111.h
+++ trunk/trunk/firmware/include/cc1111.h
@@ -6,19 +6,6 @@
 
 uint8 addr;               // temporarily store the incoming address until *after* the STATUS stage of the transaction... then assign the address.
 
-#define  EP_STATE_IDLE      0
-#define  EP_STATE_TX        1
-#define  EP_STATE_RX        2
-#define  EP_STATE_STALL     3
-
-#define USB_STATE_UNCONFIGURED 0
-#define USB_STATE_IDLE      1
-#define USB_STATE_SUSPEND   2
-#define USB_STATE_RESUME    3
-#define USB_STATE_RESET     4
-#define USB_STATE_WAIT_ADDR 5
-#define USB_STATE_BLINK     0xff
-
 
 
 SFRX(USBADDR,   0xDE00);        // Function Address
