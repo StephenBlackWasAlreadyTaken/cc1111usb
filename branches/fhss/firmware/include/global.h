366453d32303fa48e008f4a8717a4864ff9a74f8
0a71a5@gmail.com <0a71a5@gmail.com@80cae8be-53a3-4c99-4711-8fd00a4bf4d7>
2012-01-02 03:39:23 +0000

    merge cc1111usb back from trunk (helps with debugging stuffs)

--- branches/fhss/firmware/include/global.h
+++ branches/fhss/firmware/include/global.h
@@ -14,7 +14,22 @@ extern xdata u8 lastCode[2];
 //#define TRANSMIT_TEST
 //#define RECEIVE_TEST
 //////////////////////////////////////
+#define LC_USB_INITUSB                  0x2
+#define LC_MAIN_RFIF                    0xd
+#define LC_USB_DATA_RESET_RESUME        0xa
+#define LC_USB_RESET                    0xb
+#define LC_USB_EP5OUT                   0xc
+#define LC_RF_VECTOR                    0x10
+#define LC_RFTXRX_VECTOR                0x11
 
+#define LCE_USB_EP5_TX_WHILE_INBUF_WRITTEN      0x1
+#define LCE_USB_EP0_SENT_STALL                  0x4
+#define LCE_USB_EP5_OUT_WHILE_OUTBUF_WRITTEN    0x5
+#define LCE_USB_EP5_LEN_TOO_BIG                 0x6
+#define LCE_USB_EP5_GOT_CRAP                    0x7
+#define LCE_USB_EP5_STALL                       0x8
+#define LCE_USB_DATA_LEFTOVER_FLAGS             0x9
+#define LCE_RF_RXOVF                            0x10
 
 
 /* board-specific defines */
@@ -23,6 +38,7 @@ extern xdata u8 lastCode[2];
     #define LED_RED   P2_3
     #define LED_GREEN P2_4
     #define SLEEPTIMER  1100
+    #define PLATFORM_CLOCK_FREQ 26
     
  #include "immedisplay.h"
  #include "immekeys.h"
@@ -35,12 +51,14 @@ extern xdata u8 lastCode[2];
     #define LED_GREEN P1_1
     #define SLEEPTIMER  1200
     #define CC1111EM_BUTTON P1_2
+    #define PLATFORM_CLOCK_FREQ 24
 
-#else
+#elif defined CHRONOSDONGLE
     // CC1111 USB Chronos watch dongle - 24mhz
     #define LED_RED   P1_0
     #define LED_GREEN P1_0
     #define SLEEPTIMER  1200
+    #define PLATFORM_CLOCK_FREQ 24
 #endif
 
 #define LED     LED_GREEN
@@ -48,6 +66,7 @@ extern xdata u8 lastCode[2];
 
 #define REALLYFASTBLINK()        { LED=1; sleepMillis(2); LED=0; sleepMillis(10); }
 #define blink( on_cycles, off_cycles)  {LED=1; sleepMillis(on_cycles); LED=0; sleepMillis(off_cycles);}
+#define BLOCK()     { while (1) { REALLYFASTBLINK() ; usbProcessEvents(); }  }
 #define LE_WORD(x) ((x)&0xFF),((u8) (((u16) (x))>>8))
 
 /* function declarations */
