8a5492acc69f2885373805d1cb9b1406b3045e64
gvandenbosch@gmail.com <gvandenbosch@gmail.com@80cae8be-53a3-4c99-4711-8fd00a4bf4d7>
2012-01-15 13:47:41 +0000

    First send of data through the air from the GUI using the vcom

---
+++ branches/thedutchguy/clients/gui/cc1111serial.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+import sys, serial
+
+class cc1111serial():
+
+	def __init__(self):
+		self.port = "/dev/ttyACM0"
+		self.serialport = serial.Serial(self.port,115200)
+
+	def write(self,message):
+		self.serialport.write(message)
