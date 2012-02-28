a01d6221d09b91ffdbb89e46cdd8f4eacfd793c6
0a71a5@gmail.com <0a71a5@gmail.com@80cae8be-53a3-4c99-4711-8fd00a4bf4d7>
2012-02-28 00:43:37 +0000

    chipcon_usbdebug.h tweaks

--- trunk/firmware/include/chipcon_usbdebug.h
+++ trunk/firmware/include/chipcon_usbdebug.h
@@ -4,10 +4,10 @@
 #define CC1111USBDEBUG_H
 
 void debugEP0Req(u8 *pReq);
-void debug(code u8* text);
-void debughex(xdata u8 num);
-void debughex16(xdata u16 num);
-void debughex32(xdata u32 num);
+void debug(__code u8* text);
+void debughex(__xdata u8 num);
+void debughex16(__xdata u16 num);
+void debughex32(__xdata u32 num);
 
 #endif
 
