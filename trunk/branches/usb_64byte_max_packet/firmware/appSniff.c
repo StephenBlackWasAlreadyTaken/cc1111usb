8e60433a87f720ba4e468ed31b53560c85481f82
0a71a5@gmail.com <0a71a5@gmail.com@80cae8be-53a3-4c99-4711-8fd00a4bf4d7>
2012-03-03 20:33:10 +0000

    applications updated to use all of EP5's OUT buffer (since we used to skip the first 4 bytes)

--- trunk/branches/usb_64byte_max_packet/firmware/appSniff.c
+++ trunk/branches/usb_64byte_max_packet/firmware/appSniff.c
@@ -158,15 +158,15 @@ void appMainLoop(void)
 int appHandleEP5()
 {   // not used by VCOM
 #ifndef VIRTUAL_COM
-    u8 app, cmd, len;
-    xdata u8 *buf;
+    u8 app, cmd;
+    u16 len;
+    __xdata u8 *buf = &ep5.OUTbuf[0];
 
-    app = ep5.OUTbuf[4];
-    cmd = ep5.OUTbuf[5];
-    buf = &ep5.OUTbuf[6];
-    //len = (u16)*buf;    - original firmware
-    len = (u8)*buf;         // FIXME: should we use this?  or the lower byte of OUTlen?
-    buf += 2;                                               // point at the address in memory
+    app = *buf++;
+    cmd = *buf++;
+    len = (u8)*buf++;         // FIXME: should we use this?  or the lower byte of OUTlen?
+    len += (u16)((*buf++) << 8);                                               // point at the address in memory
+
     // ep5.OUTbuf should have the following bytes to start:  <app> <cmd> <lenlow> <lenhigh>
     // check the application
     //  then check the cmd
